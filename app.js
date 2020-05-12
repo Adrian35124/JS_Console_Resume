b2c00dfa05eafcb133e44533118acdc665601663
adrian35124 <adrian35124@gmail.com>
2020-05-12 15:56:38 -0500

    FINALLY FINISHED

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ function displayPosition(myName, myCareer, myDescription) {
     console.log("Description: " + myDescription );
 }
 
-
 console.log("My Interests: ");
 
 displayInterests("Video Games");
@@ -23,8 +22,33 @@ displayInterests("Anime");
 displayInterests("Spending time with my girlfirend"); 
 displayInterests("Learning how to code!"); 
 
+function displayInterests(myInterests) {
+    console.log('*  ' + myInterests);
+}
 
+console.log("My Previous Experiences: ")
 
+displayExperiences("Worked at My family's restaurant.");
+displayExperiences("Started working at chipotle became a Service Manager there.");
+displayExperiences("Worked retail at Advance Auto Parts.");
 
+function displayExperiences(myPerviousExperiences) {
+    console.log("*  " + myPerviousExperiences);
+}
 
+console.log("My Skills")
+displayMySkills('Maining Ness in SSBU', true);
+displayMySkills('Being able to code this project', true)
+displayMySkills('Getting To Elite Smash in SSBU', false)
+displayMySkills("Getting Plat in Ranked SR", false)
+displayMySkills('Getting Plat in TFT', true)
 
+function displayMySkills(mySkills, cool) {
+    if (cool === true) {
+        console.log('BAM: ' + mySkills)
+    }   else {
+        console.log('' + mySkills)
+    }
+
+    
+}
